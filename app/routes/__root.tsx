e0368de9b3077b994cf0d414bf6095e2d913828a
Brenley Dueck <brenley@crowdbotics.com>
2025-03-29 18:14:56 -0500

    Initial Tanstack Start Setup

---
+++ app/routes/__root.tsx
@@ -0,0 +1,26 @@
+import { Outlet, createRootRoute } from "@tanstack/solid-router";
+
+import appCss from "~/styles/app.css?url";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charset: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "TanStackFaster",
+      },
+    ],
+    links: [{ rel: "stylesheet", href: appCss }],
+  }),
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return <Outlet />;
+}
