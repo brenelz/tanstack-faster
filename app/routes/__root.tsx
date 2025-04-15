363d014eef12b09c5f766479962456d8fcea7740
Brenley Dueck <brenleydueck@gmail.com>
2025-04-14 22:23:42 -0500

    Await categories

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -28,7 +28,7 @@ export const Route = createRootRoute({
     // don't block loading the page
     return {
       cartPromise: getCart(),
-      categoriesPromise: getCategories()
+      categories: await getCategories()
     };
   },
   component: RootComponent,
@@ -39,23 +39,20 @@ function RootComponent() {
   const data = Route.useLoaderData();
 
   const cart = createAsync(() => data().cartPromise);
-  const categories = createAsync(() => data().categoriesPromise);
 
   return (
     <div>
       <Header cart={cart.latest} />
       <div class="pt-[85px] sm:pt-[70px]">
         <div class="flex flex-grow font-mono">
-          <Show when={categories.latest} fallback={<div class="p-4">Loading Categories...</div>}>
-            <aside class="fixed left-0 hidden w-64 min-w-64 max-w-64 overflow-y-auto border-r p-4 md:block">
-              <Sidebar categories={categories.latest} />
-            </aside>
-            <main class="min-h-[calc(100vh-113px)] flex-1 overflow-y-auto p-4 pt-0 md:pl-64">
-              <div class="w-full p-4">
-                <Outlet />
-              </div>
-            </main>
-          </Show>
+          <aside class="fixed left-0 hidden w-64 min-w-64 max-w-64 overflow-y-auto border-r p-4 md:block">
+            <Sidebar categories={data().categories} />
+          </aside>
+          <main class="min-h-[calc(100vh-113px)] flex-1 overflow-y-auto p-4 pt-0 md:pl-64">
+            <div class="w-full p-4">
+              <Outlet />
+            </div>
+          </main>
         </div>
       </div>
       <TanStackRouterDevtools />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute, getRouteApi } from "@tanstack/solid-router";
 import { For } from "solid-js";
 import { Link } from "@tanstack/solid-router";
-import { createAsync } from "@/lib/utils";
 
 export const Route = createFileRoute("/")({
   component: Home,
@@ -9,13 +8,12 @@ export const Route = createFileRoute("/")({
 
 function Home() {
   const data = getRouteApi("__root__").useLoaderData();
-  const categories = createAsync(() => data().categoriesPromise);
 
   return (
     <div class="w-full space-y-12">
       <div class="space-y-4">
         <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-          <For each={categories.latest}>
+          <For each={data().categories}>
             {(category) => (
               <Link
                 to="/categories/$category"
